7f1c6132b20050cdcce1dd2d9877da87e5f3dd65
KrishKoria <krishkoria2004@gmail.com>
2023-07-06 22:15:43 +0530

    Added bunch of codes

---
+++ average height.py
@@ -0,0 +1,6 @@
+student_heights = input("Input a list of student heights ").split(",")
+total = 0
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+    total += student_heights[n]
+print(total / 5)
