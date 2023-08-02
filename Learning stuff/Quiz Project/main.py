c72938e4aa02c186d7a92c2e5cbc4fff28b83dca
KrishKoria <krishkoria2004@gmail.com>
2023-08-02 18:49:33 +0530

    Completed Quiz Project

--- Learning stuff/Quiz Project/main.py
+++ Learning stuff/Quiz Project/main.py
@@ -5,8 +5,8 @@ from question_model import Question
 question_bank = []
 
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_q = Question(text=question_text, answer=question_answer)
     question_bank.append(new_q)
 
@@ -15,4 +15,8 @@ for question in question_data:
 #     print(q.answer)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+while quiz.more_questions():
+    quiz.next_question()
+
+print("Congratulations!!,You've Completed the Quiz")
+print(f"Your final Score is {quiz.score}")
--- Learning stuff/Quiz Project/quiz_brain.py
+++ Learning stuff/Quiz Project/quiz_brain.py
@@ -3,8 +3,23 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.score = 0
+
+    def more_questions(self):
+        return self.question_number < len(self.question_list)
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         ans = input(f"Q.{self.question_number}:{current_question.text} (True/False): ").lower()
+        self.checking_answer(ans, current_question.answer)
+
+    def checking_answer(self, given_ans, correct_answer):
+        if given_ans == correct_answer.lower():
+            print("Correct Answer!!")
+            self.score += 1
+        else:
+            print("Wrong Answer")
+        print(f"The correct Answer is {correct_answer}")
+        print(f"Your Current Score is {self.score}/{self.question_number}")
+        print("\n")
