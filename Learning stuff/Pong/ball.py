a57f7a209d63a19b0b0386891f6103b9d82e5701
KrishKoria <krishkoria2004@gmail.com>
2023-08-24 22:37:19 +0530

    Compeleted Pong

--- Learning stuff/Pong/ball.py
+++ Learning stuff/Pong/ball.py
@@ -7,5 +7,21 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.speed = 0.1
+
     def move(self):
+        self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+
+    def bounce_upper_wall(self):
+        self.y_move *= -1
+
+    def bounce_paddle(self):
+        self.x_move *= -1
+        self.speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.speed = 0.1
 
--- Learning stuff/Pong/main.py
+++ Learning stuff/Pong/main.py
@@ -1,24 +1,39 @@
 from turtle import Turtle, Screen
 from Paddle import Paddle
 from ball import Ball
-
+from scoreboard import Scoreboard
+import time
 screen = Screen()
 screen.setup(800, 800)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 ball = Ball()
-left = Paddle(350, 0)
-right = Paddle(-350, 0)
+right = Paddle(350, 0)
+left = Paddle(-350, 0)
 screen.listen()
+score_board = Scoreboard()
+screen.onkeypress(right.go_up, 'Up')
+screen.onkeypress(right.go_down, 'Down')
 
-screen.onkeypress(left.go_up, 'Up')
-screen.onkeypress(left.go_down, 'Down')
-
-screen.onkeypress(right.go_up, 'w')
-screen.onkeypress(right.go_down, 's')
+screen.onkeypress(left.go_up, 'w')
+screen.onkeypress(left.go_down, 's')
 
 game_on = True
 while game_on:
+    time.sleep(ball.speed)
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 380 or ball.ycor() < -380:
+        ball.bounce_upper_wall()
+    if ball.distance(right) < 50 and ball.xcor() > 320 or ball.distance(left) < 50 and ball.xcor() < -320:
+        ball.bounce_paddle()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score_board.l_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score_board.r_point()
 screen.exitonclick()
