1c4e3ab7ea9a5b77a35283a24f6026735ea96d76
KrishKoria <krishkoria2004@gmail.com>
2023-08-26 13:45:57 +0530

    Python college lab 3 assignment

--- College Python Stufff/python lab-3/Q7.py
+++ College Python Stufff/python lab-3/Q7.py
@@ -1,8 +1,7 @@
 a = ""
-for Row in range(0, 7):
-    for Col in range(0, 7):
-        if ((Row == 0 or Row == 3 or Row == 6) and 1 < Col < 5) or (
-                Col == 1 and (Row == 1 or Row == 2 or Row == 6)) or (Col == 5 and (Row == 0 or Row == 4 or Row == 5)):
+for Row in range(0,7):
+    for Col in range(0,7):
+        if ((Row == 0 or Row == 3 or Row == 6) and 1 < Col < 5) or (Col == 1 and (Row == 1 or Row == 2 or Row == 6)) or (Col == 5 and (Row == 0 or Row == 4 or Row == 5)):
             a = a + "*"
         else:
             a = a + " "
